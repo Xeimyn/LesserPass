2c56a8f3e3688df542b27784d0b87775908f42c2
Simon <gitcode@jcms.dev>
2025-04-05 00:19:55 +0200

    1 step closer, its adding a button with img now poggies

--- content/autofill.js
+++ content/autofill.js
@@ -8,7 +8,31 @@ function detectPasswordElements() {
 
 		const finalPasswordElement = passwordElements[0];
 		finalPasswordElement.style.border = "5px red solid"
-		observer.disconnect()
+
+		// Create a button with an img inside that uses assets/icon.svg and position it to the top right
+		// of the finalPasswordElement with an offset of 5px from the top and right
+		const autoFillButton = document.createElement("button");
+		autoFillButton.style.position = "absolute";
+		autoFillButton.style.top = `${finalPasswordElement.offsetTop + 5}px`;
+		autoFillButton.style.left = `${finalPasswordElement.offsetLeft + finalPasswordElement.offsetWidth - 5}px`;
+		autoFillButton.style.padding = "0";
+		autoFillButton.style.border = "none";
+		autoFillButton.style.background = "black";
+		autoFillButton.style.cursor = "pointer";
+		autoFillButton.style.width = "20px";
+		autoFillButton.style.height = "20px";
+
+		const autoFillIcon = document.createElement("img");
+		autoFillIcon.src = chrome.runtime.getURL("assets/icon.svg");
+		autoFillIcon.alt = "Autofill";
+		autoFillIcon.style.width = "20px";
+		autoFillIcon.style.height = "20px";
+
+		autoFillButton.appendChild(autoFillIcon);
+
+
+		finalPasswordElement.parentElement.appendChild(autoFillButton);
+		observer.disconnect(); // Stop observing once we find the password field
 	}
 }
 
