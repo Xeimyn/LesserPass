59fd8cb3290c771cdff70470b2ecab181da50154
Xeimyn <github@jcms.dev>
2025-05-05 09:55:39 +0200

    ♻️ Quick refactor of the main functionallity. 🚧 Actually prepared now for autofill.

--- background/background.js
+++ background/background.js
@@ -1,6 +1,24 @@
-// Listen for messages from extension button to open popup
+let openedViaButton
+let iSupplyThisURL
+
+// Listen for messages from injected extension button to open popup
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
 	if (request.action === "openPopup") {
+		openedViaButton = true
+		iSupplyThisURL = sender.url
 		await chrome.action.openPopup()
+
+		setTimeout(() => {
+			null
+		}, 3000);
+
+
+	} else if (request.action == "getOpenedViaButton") {
+		if (openedViaButton == undefined) {
+			openedViaButton = false
+		}
+		sendResponse([openedViaButton,iSupplyThisURL])
+	} else if (request.action == "resetOpenedViaButton") {
+		openedViaButton = false
 	}
 });
