63d43240589bbff6baccab9a74acbbf1475d2157
Simon <gitcode@jcms.dev>
2025-06-01 02:55:11 +0200

    Prepare stuff

--- background/background.js
+++ background/background.js
@@ -22,3 +22,33 @@ chrome.runtime.onMessage.addListener(async function (request, sender, sendRespon
 		openedViaButton = false
 	}
 });
+
+chrome.runtime.onInstalled.addListener(async (details) => {
+  const currentVersion = chrome.runtime.getManifest().version;
+  const [currMajor, currMinor, currPatch] = currentVersion.split('.').map(Number);
+
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: chrome.runtime.getURL("/settings/settings.html") });
+
+    // Store current version on install
+    chrome.storage.local.set({ lesserpassVersion: currentVersion });
+  }
+
+  if (details.reason === 'update') {
+    const { lesserpassVersion: prevVersion } = await chrome.storage.local.get('lesserpassVersion');
+
+    if (prevVersion) {
+      const [prevMajor, prevMinor, prevPatch] = prevVersion.split('.').map(Number);
+
+      const majorOrMinorChanged =
+        currMajor !== prevMajor || currMinor !== prevMinor;
+
+      if (majorOrMinorChanged) {
+        chrome.tabs.create({ url: chrome.runtime.getURL("/changelog/changelog.html") });
+      }
+    }
+
+    // Update stored version after update
+    chrome.storage.local.set({ lesserpassVersion: currentVersion });
+  }
+});
